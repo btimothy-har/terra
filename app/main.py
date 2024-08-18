8dbfd1b4e0a96d0f2ce0c1fefbb000cdadc4f0c9
Brian Timothy Har <62061780+btimothy-har@users.noreply.github.com>
2024-08-18 22:52:47 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -230,8 +230,7 @@ if __name__ == "__main__":
                     )
 
                 with message_container:
-                    status_elem = st.empty()
-                    with status_elem.status("Thinking...", expanded=True) as status:
+                    with st.status("Thinking...", expanded=True) as status:
                         response = invoke_graph()
                         time_taken = datetime.now(timezone.utc) - message_time
                         status.update(
@@ -239,7 +238,6 @@ if __name__ == "__main__":
                             expanded=False,
                             state="complete",
                         )
-                    status_elem.empty()
                     full_response = st.write_stream(response["output"])
 
             new_asst_message = st.session_state.current_thread.append(
