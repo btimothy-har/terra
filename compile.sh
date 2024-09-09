c75f61e89d2c6731fe97b2d9922b856588f6a500
Brian Timothy Har <62061780+btimothy-har@users.noreply.github.com>
2024-09-09 10:55:41 +0800

    Update compiler (#48)

--- compile.sh
+++ compile.sh
@@ -2,7 +2,9 @@ eval "uv pip compile app/requirements.in -o app/requirements.txt"
 eval "uv pip compile api/requirements.in -o api/requirements.txt"
 
 eval "uv sync"
+eval "uv venv"
 
 eval "uv pip install -r app/requirements.txt -r api/requirements.txt"
 
-echo "Compilation complete"
+echo "Project compiled successfully."
+echo "Activate venv with 'source .venv/bin/activate'"
