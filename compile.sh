9784a3c9ecf46ebcacaa5bf06bfe0ff67e35ae99
btimothy-har <62061780+btimothy-har@users.noreply.github.com>
2024-09-11 16:13:33 +0800

    Update compile script

--- compile.sh
+++ compile.sh
@@ -1,11 +1,13 @@
-eval "uv pip compile app/requirements.in api/requirements.in jobs/requirements.in -o requirements.txt"
+#!/bin/bash
 
-eval "uv pip compile app/requirements.in -o app/requirements.txt"
-eval "uv pip compile api/requirements.in -o api/requirements.txt"
-eval "uv pip compile jobs/requirements.in -o jobs/requirements.txt"
+uv pip compile api/requirements.in -o api/requirements.txt
+uv pip compile app/requirements.in -o app/requirements.txt
+uv pip compile jobs/requirements.in -o jobs/requirements.txt
 
-eval "uv sync"
+uv sync
 
-eval "uv pip install -r requirements.txt"
+uv pip install -r api/requirements.txt
+uv pip install -r app/requirements.txt
+uv pip install -r jobs/requirements.txt
 
 echo "Compilation complete." 
