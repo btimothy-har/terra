5e32dc2677489e8aa2233f6e463b0849e147f595
btimothy-har <62061780+btimothy-har@users.noreply.github.com>
2024-10-24 19:42:25 +0800

    Update run.sh script

--- jobs/run.sh
+++ jobs/run.sh
@@ -3,15 +3,17 @@ docker build -t terra_jobs:latest .
 docker run \
   --rm \
   --name terra_jobs_$(date +%s) \
-  --env-file .env \
+  -e ELL_DB=ell_studio \
+  -e FARGS_LLM_TOKEN_LIMIT=1000000 \
+  -e POSTGRES_DB=terra \
   -e POSTGRES_USER=${POSTGRES_USER} \
   -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
+  -e NEO4J_PASSWORD=${NEO4J_PASSWORD} \
   -e NEWS_API_KEY=${NEWS_API_KEY} \
   -e OPENROUTER_API_KEY=${OPENROUTER_API_KEY} \
   -e OPENAI_API_KEY=${OPENAI_API_KEY} \
   --network services_net_postgres \
   --network services_net_redis \
   --network services_net_neo4j \
-  --volume terra_llm_logs:/src/logdir \
   terra_jobs:latest \
   "$@"
